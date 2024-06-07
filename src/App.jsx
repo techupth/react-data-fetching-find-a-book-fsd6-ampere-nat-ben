c437076d28fa39c7b8c6159ae0b86378c84fbf59
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-06-07 11:40:45 +0700

    finished assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,58 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { DebounceInput } from "react-debounce-input";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [query, setQuery] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChangeQuery = (e) => {
+    if (e.target.value === "") {
+      setSearchResult([]);
+    }
+    setQuery(e.target.value);
+  };
+  useEffect(() => {
+    const initFetch = async () => {
+      try {
+        setIsLoading(true);
+        let url = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
+        let response = await axios.get(url);
+        setSearchResult(response.data.items);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (query) {
+      initFetch();
+    }
+  }, [query]);
+
+  const displaySearchResult = () => {
+    return searchResult.map((item, index) => {
+      return <p key={index}>{item.volumeInfo.title}</p>;
+    });
+  };
+  return (
+    <>
+      <h1>Find a Book</h1>
+      {/* <input type="text" value={query} onChange={handleChangeQuery} /> */}
+      <DebounceInput
+        type="text"
+        debounceTimeout={300}
+        onChange={handleChangeQuery}
+        placeholder="Search book title"
+      />
+      <br></br>
+      {isLoading ? "Loading..." : displaySearchResult()}
+    </>
+  );
 }
 
 export default App;
